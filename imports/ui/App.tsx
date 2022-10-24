8ddefa38eeec6a1f2db07db3e3dfa6db9a2e9053
Armaghan Khan <khanhassan918@ymail.com>
2022-10-24 12:39:12 +0500

    Step 3: added form for adding task

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Task } from './Task';
 import { ITask, TasksCollection } from '../api/tasks';
+import TaskForm from '../ui/forms/TaskForm';
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => TasksCollection.find({}, {sort: {createdAt: 1}}).fetch());
 
   const createTask = (task: ITask) => {
     return <Task task={task} key={task._id}/>;
@@ -12,6 +13,9 @@ export const App = () => {
 
   return (<div>
     <h1>Welcome to Meteor!</h1>
+
+    <TaskForm/>
+
     <ul>
       { tasks.map((task) => createTask(task as ITask))}
     </ul>
---
+++ imports/ui/forms/TaskForm.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { TasksCollection, ITask } from '/imports/api/tasks';
+
+const TaskForm = () => {
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (!text) return;
+
+    TasksCollection.insert({
+      text: text.trim(),
+      createdAt: new Date()
+    } as ITask);
+
+    setText("");
+  };
+
+  return (
+    <form className="task-form">
+      <input
+        type="text"
+        placeholder="Type to add new tasks"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button type="submit" onClick={handleSubmit}>Add Task</button>
+    </form>
+  );
+};
+
+export default TaskForm;
